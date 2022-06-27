eca1077fd3f188610858b7ead288368da6d229ab
Hieunm54 <hieunm.542@gmail.com>
2022-06-27 13:12:20 +0700

    fix: fix bug and schema

--- fm_api/models/match-model.js
+++ fm_api/models/match-model.js
@@ -3,42 +3,50 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const matchSchema = new Schema({
-    team1: {
-        teamId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Team",
-        },
-        score: Number,
-        required: true
-    },
-    team2: {
-        teamId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Team",
-        },
-        score: Number,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['none', 'pending', 'conflict', 'confirm'],
-        required: true
-    },
-    matchRecord: [
-        {
-            memberId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Member",
-            },
-            minute: Number
-        }
-    ],
-    time: {
-        type: Date
-    },
-    area:{
-        type: String
-    }
+	team1: {
+		teamId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Team",
+		},
+		score: Number,
+		verifyScore: Number, // from rival ->
+		//check if score === verifyScore -> resolve
+		// score !== verifyScore -> conflict
+		// verify score === null -> pending input
+		required: true,
+	},
+	team2: {
+		teamId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Team",
+		},
+		score: Number,
+		verifyScore: Number, // from rival
+		//check if score === verifyScore -> resolve
+		// score !== verifyScore -> conflict
+		// verify score === null -> pending input
+		required: true,
+	},
+	status: {
+		type: String,
+		enum: ["none", "pending", "conflict", "confirm"],
+		required: true,
+	},
+	matchRecord: [
+		{
+			memberId: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Member",
+			},
+			minute: Number,
+		},
+	],
+	time: {
+		type: Date,
+	},
+	area: {
+		type: String,
+	},
 });
 
-export default mongoose.model('Match', matchSchema);
+export default mongoose.model("Match", matchSchema);
