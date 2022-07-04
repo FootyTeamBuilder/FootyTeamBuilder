0d68099246c496d9c1c0edf15fcfcb622e3b6e1e
tranducanh <tdanh041100@gmail.com>
2022-07-04 10:18:04 +0700

    fix team controller

--- fm_api/controllers/team-controller.js
+++ fm_api/controllers/team-controller.js
@@ -6,265 +6,270 @@ import jwt from "jsonwebtoken";
 import ROLE from "../utils/enums.js";
 
 class TeamController {
-	//[POST] /team/create
-	create = async (req, res, next) => {
-		try {
-			const userId = req.userId;
-			//create new user
-			const newTeam = await teamModel.create({
-				name: req.body.name,
-				description: req.body.description,
-				level: req.body.level,
-				age: {
-					minAge: req.body.minAge,
-					maxAge: req.body.maxAge,
-				},
-			});
-			const newMember = await memberModel.create({
-				teamId: newTeam._id,
-				userId: userId,
-				role: ROLE.CAPTAIN,
-			});
-			return res.status(201).json({
-				message: "Create New Team Successful",
-				id: newTeam._id,
-				name: newTeam.name,
-			});
-		} catch (error) {
-			if (!error.statusCode) {
-				error.statusCode = 500;
-			}
-			next(error);
+  //[POST] /team/create
+  create = async (req, res, next) => {
+    try {
+      const userId = req.userId;
+      //create new user
+      const newTeam = await teamModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        level: req.body.level,
+        age: {
+          minAge: req.body.minAge,
+          maxAge: req.body.maxAge,
+        },
+      });
+      const newMember = await memberModel.create({
+        teamId: newTeam._id,
+        userId: userId,
+        role: ROLE.CAPTAIN,
+      });
+      return res.status(201).json({
+        message: "Create New Team Successful",
+        id: newTeam._id,
+        name: newTeam.name,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    }
+  };
+
+  // [PUT] /team/edit
+  edit = async (req, res, next) => {
+    const userId = req.userId;
+
+    const data = req.body;
+    const teamId = req.params.teamId;
+    console.log(userId);
+    try {
+      const foundMember = await memberModel.findOne({
+        userId: userId,
+        teamId: teamId,
+      });
+      console.log(foundMember);
+
+      if (foundMember.role != ROLE.CAPTAIN) {
+        return res.status(403).json({
+          message: "Not permitted",
+        });
+      }
+
+      const foundTeam = await teamModel.findById(teamId);
+
+      Object.keys(data).reduce((team, key) => {
+        //update embedded minAge maxAge
+        if (!team[key]) {
+          console.log(team["age"][key]);
+          team["age"][key] = data[key];
+          return team;
+        }
+        team[key] = data[key];
+
+        return team;
+      }, foundTeam);
+
+      console.log(foundTeam);
+      await foundTeam.save();
+      return res.status(201).json({
+        message: "Update info successful",
+        id: foundTeam._id,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    }
+  };
+
+  // [GET] /team/list
+  listTeam = async (req, res, next) => {
+    // attract keyword
+    const keyword = req.params.keyword;
+    // res.send({ message: "list team", keyword });
+    let allTeam = [];
+    // if no keyword provided -> findAll
+    if (!keyword) {
+      allTeam = await teamModel.find({});
+    } else {
+      allTeam = await teamModel.find({
+        name: { $regex: keyword, $options: "i" },
+      });
+    }
+
+    res.status(201).send({
+      message: "Fetch list team successful",
+      data: allTeam,
+    });
+  };
+  viewTeam = async (req, res, next) => {
+    try {
+      const teamId = req.params.teamId;
+      const foundTeam = await teamModel.findById(teamId);
+      //search in member
+      const captain = await memberModel.findOne({
+        teamId: teamId,
+        role: ROLE.CAPTAIN,
+      });
+      const members = await memberModel.find({
+        teamId: teamId,
+        role: ROLE.MEMBER,
+      });
+      //search member in user
+      const captainUser = await userModel.findById(captain.userId);
+      let memberUsers = [];
+      for (var member of members) {
+        if (member.isExistUser) {
+          let memberUser = await userModel.findById(member.userId);
+          memberUsers.push({
+			member: member,
+			info: memberUser
+		  });
+        } else {
+			memberUsers.push({
+				member: member
+			})
 		}
-	};
+      }
+      return res.status(201).json({
+        team: foundTeam,
+        captain: captainUser,
+        members: memberUsers,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    }
+  };
 
-	// [PUT] /team/edit
-	edit = async (req, res, next) => {
-		const userId = req.userId;
+  createMember = async (req, res, next) => {
+    const userId = req.userId;
 
-		const data = req.body;
-		const teamId = req.params.teamId;
-		console.log(userId);
-		try {
-			const foundMember = await memberModel.findOne({
-				userId: userId,
-				teamId: teamId,
-			});
-			console.log(foundMember);
+    const data = req.body;
+    const teamId = req.params.teamId;
+    try {
+      const foundMember = await memberModel.findOne({
+        userId: userId,
+        teamId: teamId,
+      });
+      if (foundMember.role != ROLE.CAPTAIN) {
+        return res.status(403).json({
+          message: "Not permitted!!",
+        });
+      }
+      data.teamId = teamId;
+      const foundTeam = await teamModel.findById(teamId);
 
-			if (foundMember.role != ROLE.CAPTAIN) {
-				return res.status(403).json({
-					message: "Not permitted",
-				});
-			}
+      if (data["isExistUser"]) {
+        const existMember = await userModel.findOne({ email: data.email });
+        data.userId = existMember._id;
+      }
+      console.log(data);
+      const newMember = await memberModel.create(data);
 
-			const foundTeam = await teamModel.findById(teamId);
+      return res.status(201).json({
+        message: "Create member successful!",
+        member: newMember,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    }
+  };
 
-			Object.keys(data).reduce((team, key) => {
-				//update embedded minAge maxAge
-				if (!team[key]) {
-					console.log(team["age"][key]);
-					team["age"][key] = data[key];
-					return team;
-				}
-				team[key] = data[key];
+  viewMember = async (req, res, next) => {
+    const memberId = req.params.memberId;
+    try {
+      const foundMember = await memberModel.findById(memberId);
+      let userInfo;
+      if (foundMember.isExistUser) {
+        userInfo = await userModel.findById(foundMember.userId);
+      }
 
-				return team;
-			}, foundTeam);
+      return res.status(201).json({
+        member: foundMember,
+        info: userInfo,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    }
+  };
 
-			console.log(foundTeam);
-			await foundTeam.save();
-			return res.status(201).json({
-				message: "Update info successful",
-				id: foundTeam._id,
-			});
-		} catch (error) {
-			if (!error.statusCode) {
-				error.statusCode = 500;
-			}
-			next(error);
-		}
-	};
+  updateMember = async (req, res, next) => {
+    const userId = req.userId;
+    const data = req.body;
+    const memberId = req.params.memberId;
+    const teamId = req.params.teamId;
+    try {
+      const foundCaptain = await memberModel.findOne({
+        userId: userId,
+        teamId: teamId,
+      });
 
-	// [GET] /team/list
-	listTeam = async (req, res, next) => {
-		// attract keyword
-		const keyword = req.params.keyword;
-		// res.send({ message: "list team", keyword });
-		let allTeam = [];
-		// if no keyword provided -> findAll
-		if (!keyword) {
-			allTeam = await teamModel.find({});
-		} else {
-			allTeam = await teamModel.find({
-				name: { $regex: keyword, $options: "i" },
-			});
-		}
+      if (foundCaptain.role != ROLE.CAPTAIN) {
+        return res.status(403).json({
+          message: "Not permitted",
+        });
+      }
 
-		res.status(201).send({
-			message: "Fetch list team successful",
-			data: allTeam,
-		});
-	};
-	viewTeam = async (req, res, next) => {
-		try {
-			const teamId = req.params.teamId;
-			const foundTeam = await teamModel.findById(teamId);
-			//search in member
-			const captain = await memberModel.findOne({
-				teamId: teamId,
-				role: ROLE.CAPTAIN,
-			});
-			const members = await memberModel.find({
-				teamId: teamId,
-				role: ROLE.MEMBER,
-			});
-			//search member in user
-			const captainUser = await userModel.findById(captain.userId);
-			let memberUsers = [];
-			for (var member of members) {
-				let memberUser = await userModel.findById(member.userId);
-				memberUsers.push(memberUser);
-			}
-			return res.status(201).json({
-				team: foundTeam,
-				captain: captainUser,
-				members: memberUsers,
-			});
-		} catch (error) {
-			if (!error.statusCode) {
-				error.statusCode = 500;
-			}
-			next(error);
-		}
-	};
+      const foundMember = await memberModel.findById(memberId);
 
-	createMember = async (req, res, next) => {
-		const userId = req.userId;
+      Object.keys(data).reduce((member, key) => {
+        member[key] = data[key];
+        return member;
+      }, foundMember);
 
-		const data = req.body;
-		const teamId = req.params.teamId;
-		try {
-			const foundMember = await memberModel.findOne({
-				userId: userId,
-				teamId: teamId,
-			});
-			if (foundMember.role != ROLE.CAPTAIN) {
-				return res.status(403).json({
-					message: "Not permitted!!",
-				});
-			}
-			data.teamId = teamId;
-			const foundTeam = await teamModel.findById(teamId);
+      console.log(foundMember);
+      await foundMember.save();
+      return res.status(201).json({
+        message: "Update info successful",
+        member: foundMember,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    }
+  };
 
-			if (data['isExistUser']) {
-				const existMember = await userModel.findOne({ email: data.email });
-				data.userId = existMember._id;
-			}
-			console.log(data);
-			const newMember = await memberModel.create(
-				data
-			);
+  deleteMember = async (req, res, next) => {
+    const userId = req.userId;
+    const memberId = req.params.memberId;
+    const teamId = req.params.teamId;
+    try {
+      const foundCaptain = await memberModel.findOne({
+        userId: userId,
+        teamId: teamId,
+      });
 
-			return res.status(201).json({
-				message: "Create member successful!",
-				member: newMember
-			});
-		} catch (error) {
-			if (!error.statusCode) {
-				error.statusCode = 500;
-			}
-			next(error);
-		}
-	};
-
-	viewMember = async (req, res, next) => {
-		const memberId = req.params.memberId;
-		try {
-			const foundMember = await memberModel.findById(memberId);
-			let userInfo;
-			if (foundMember.isExistUser) {
-				userInfo = await userModel.findById(foundMember.userId);
-
-			}
-
-			return res.status(201).json({
-				member: foundMember,
-				info: userInfo,
-			});
-		} catch (error) {
-			if (!error.statusCode) {
-				error.statusCode = 500;
-			}
-			next(error);
-		}
-	};
-
-	updateMember = async (req, res, next) => {
-		const userId = req.userId;
-		const data = req.body;
-		const memberId = req.params.memberId;
-		const teamId = req.params.teamId;
-		try {
-			const foundCaptain = await memberModel.findOne({
-				userId: userId,
-				teamId: teamId,
-			});
-
-			if (foundCaptain.role != ROLE.CAPTAIN) {
-				return res.status(403).json({
-					message: "Not permitted",
-				});
-			}
-
-			const foundMember = await memberModel.findById(memberId);
-
-			Object.keys(data).reduce((member, key) => {
-				member[key] = data[key];
-				return member;
-			}, foundMember);
-
-			console.log(foundMember);
-			await foundMember.save();
-			return res.status(201).json({
-				message: "Update info successful",
-				member: foundMember,
-			});
-		} catch (error) {
-			if (!error.statusCode) {
-				error.statusCode = 500;
-			}
-			next(error);
-		}
-	};
-
-	deleteMember = async (req, res, next) => {
-		const userId = req.userId;
-		const memberId = req.params.memberId;
-		const teamId = req.params.teamId;
-		try {
-			const foundCaptain = await memberModel.findOne({
-				userId: userId,
-				teamId: teamId,
-			});
-
-			if (foundCaptain.role != ROLE.CAPTAIN) {
-				return res.status(403).json({
-					message: "Not permitted",
-				});
-			}
-			await memberModel.deleteOne({_id: memberId});
-			return res.status(201).json({
-				message: "Delete member successful!!",
-			});
-		} catch (error) {
-			if (!error.statusCode) {
-				error.statusCode = 500;
-			}
-			next(error);
-		}
-	};
-
+      if (foundCaptain.role != ROLE.CAPTAIN) {
+        return res.status(403).json({
+          message: "Not permitted",
+        });
+      }
+      await memberModel.deleteOne({ _id: memberId });
+      return res.status(201).json({
+        message: "Delete member successful!!",
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    }
+  };
 }
 
 export default TeamController;
