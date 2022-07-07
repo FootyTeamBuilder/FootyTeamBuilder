9d4cfb421181e50096c6c7fcda82fd029350d5c1
Hieunm54 <hieunm.542@gmail.com>
2022-07-07 11:14:59 +0700

    fix team model bug

--- fm_api/models/team-model.js
+++ fm_api/models/team-model.js
@@ -3,51 +3,61 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const teamSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    level: {
-        type: String,
-        enum: ['vui vẻ', 'yếu', 'trung bình', 'khá', 'bán chuyên', 'chuyên nghiệp'],
-        default: 'vui vẻ'
-    },
-    age: {
-        minAge: { type: Number, min: 0 },
-        maxAge: { type: Number, min: 0 },
-    },
-    competitionHistory: [//
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Matchs"
-        }
-    ],
-    kits: {
-        type: String,
-        required: false
-    },
-    logo: {
-        type: String,
-        required: false
-    },
-    area: {
-        type: String,
-        required: false
-    },
-    time: {
-        type: Date,
-        required: false,
-    },
-    comments: [//
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comments"
-        }
-    ]
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		default: "",
+		required: false,
+	},
+	level: {
+		type: String,
+		enum: [
+			"vui vẻ",
+			"yếu",
+			"trung bình",
+			"khá",
+			"bán chuyên",
+			"chuyên nghiệp",
+		],
+		default: "vui vẻ",
+	},
+	age: {
+		minAge: { type: Number, min: 0 },
+		maxAge: { type: Number, min: 0 },
+	},
+	competitionHistory: [
+		//
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Matchs",
+		},
+	],
+	kits: {
+		type: String,
+		required: false,
+	},
+	logo: {
+		type: String,
+		required: false,
+	},
+	area: {
+		type: String,
+		required: false,
+	},
+	time: {
+		type: Date,
+		required: false,
+	},
+	comments: [
+		//
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Comments",
+		},
+	],
 });
 
-export default mongoose.model('Team', teamSchema);
+export default mongoose.model("Team", teamSchema);
